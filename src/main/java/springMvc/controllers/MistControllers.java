cfa4e4590f2be9fef8410fc5591ad24a8269e585
yrysNM <yrys.n3m@gmail.com>
2023-03-17 23:32:32 +0600

    HttpServletRequest and @RequestParam

--- src/main/java/springMvc/controllers/MistControllers.java
+++ src/main/java/springMvc/controllers/MistControllers.java
@@ -2,10 +2,13 @@ package springMvc.controllers;
 
 import java.util.Arrays;
 
+// import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import springMvc.entity.Heroes;
 
@@ -14,7 +17,14 @@ import springMvc.entity.Heroes;
 public class MistControllers {
 
     @GetMapping("/heroes")
-    public String getHeroes(Model model) {
+    public String getHeroes(@RequestParam("name") String name, @RequestParam("rang") String rang, Model model) {
+
+        // first variant HttpServletRequest request
+        // String name = request.getParameter("name");
+        // String rang = request.getParameter("rang");
+
+        System.out.println(name + " " + rang);
+
         model.addAttribute("info", "Hello mist village");
         model.addAttribute("heroes", Arrays.asList(
                 new Heroes(1, "Gara", "kage"),
